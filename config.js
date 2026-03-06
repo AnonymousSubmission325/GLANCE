6ee1f67603893bbe6fec484b4a65c25238125fd0
AnonymousSubmission325 <rbuchmueller@dbvis.inf.uni-konstanz.de>
2026-03-06 21:40:06 +0100

    changed deployment

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.GLANCE_CONFIG = {
-  API_BASE_URL: "https://glance-r3t49l404-anonymoussubmission325s-projects.vercel.app"
+  API_BASE_URL: "https://glance-1uu6y1xqj-anonymoussubmission325s-projects.vercel.app"
 };
